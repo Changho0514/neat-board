ac0352de8815692e5f51fab59c3990b2007a0f66
Changho0514 <chh9450@naver.com>
2023-12-02 15:57:57 +0900

    #6 - 스프링 부트 프로젝트 시작
    jdk 충돌이 일어나서 failed 에러가 뜸.
    새로 폴더를 파서 초기화함
    
    스프링 부트 프로젝트 초기화
    
    * 롬복
    * 스프링 웹
    * 액츄에이터
    * 데브툴즈

---
+++ src/test/java/com/example/neatboard1/NeatBoard1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.neatboard1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeatBoard1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
