25a008d4d4f0fd0ec962478b1c064c735d51b96e
Changho0514 <chh9450@naver.com>
2023-12-11 22:02:16 +0900

    #16 - 로그인 컨트롤러 테스트 + 시큐리티 설정을 컨트롤러에 반영
    스프링 시큐리티가 자동 생성해준 `/login`컨트롤러를 테스트, 그리고 모든 컨트롤러가 시큐리티 설정을 부를 수 있도록 `@Import` 추가

--- src/test/java/com/example/neatboard1/controller/ArticleControllerTest.java
+++ src/test/java/com/example/neatboard1/controller/ArticleControllerTest.java
@@ -1,21 +1,24 @@
 package com.example.neatboard1.controller;
 
+import com.example.neatboard1.config.SecurityConfig;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @DisplayName("View 컨트롤러 - 게시글")
+@Import(SecurityConfig.class)
 @WebMvcTest(ArticleController.class)
 class ArticleControllerTest {
     private final MockMvc mvc;
+
     public ArticleControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
     }
