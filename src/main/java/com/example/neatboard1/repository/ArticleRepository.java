df221de492f07c74ec329363d2c193de3b0419eb
Changho0514 <chh9450@naver.com>
2023-12-14 19:46:16 +0900

    #19 - 서비스 로직 테스트 코드 업데이트 및 DTO 설계, 쿼리 메소드 작성
    테스트 코드를 완성하고 연관된 DTO 를 설계함.
    테스트를 필요로 하는 repository 쿼리 메서드도 작성

--- src/main/java/com/example/neatboard1/repository/ArticleRepository.java
+++ src/main/java/com/example/neatboard1/repository/ArticleRepository.java
@@ -4,6 +4,8 @@ import com.example.neatboard1.domain.Article;
 import com.example.neatboard1.domain.QArticle;
 import com.querydsl.core.types.dsl.DateTimeExpression;
 import com.querydsl.core.types.dsl.StringExpression;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
@@ -15,6 +17,8 @@ public interface ArticleRepository extends
         JpaRepository<Article, Long>,
         QuerydslPredicateExecutor<Article>,
         QuerydslBinderCustomizer<QArticle> {
+
+    Page<Article> findByTitle(String title, Pageable pageable);
     @Override
     default void customize(QuerydslBindings bindings, QArticle root) {
         bindings.excludeUnlistedProperties(true); // 리스팅 하지 않은 프로퍼티는 막아놓는다.
---
+++ src/main/java/com/example/neatboard1/service/ArticleService.java
@@ -0,0 +1,39 @@
+package com.example.neatboard1.service;
+
+import com.example.neatboard1.domain.type.SearchType;
+import com.example.neatboard1.dto.ArticleDto;
+import com.example.neatboard1.dto.ArticleWithCommentsDto;
+import com.example.neatboard1.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class ArticleService {
+
+    private final ArticleRepository articleRepository;
+
+    @Transactional(readOnly = true)
+    public Page<ArticleDto> searchArticles(SearchType searchType, String searchKeyword, Pageable pageable) {
+        return Page.empty();
+    }
+
+    @Transactional(readOnly = true)
+    public ArticleWithCommentsDto getArticle(Long articleId) {
+        return null;
+    }
+
+    public void saveArticle(ArticleDto dto) {
+    }
+
+    public void updateArticle(ArticleDto dto) {
+    }
+
+    public void deleteArticle(long articleId) {
+    }
+
+}
