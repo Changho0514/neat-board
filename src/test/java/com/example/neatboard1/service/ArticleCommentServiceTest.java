9bda58e16527e670e842c34cbe02e9ad58755d11
Changho0514 <chh9450@naver.com>
2023-12-28 09:46:43 +0900

    #36 - 댓글 저장, 삭제 기능을 위한 컨트롤러와 서비스 코드 작성

--- src/test/java/com/example/neatboard1/service/ArticleCommentServiceTest.java
+++ src/test/java/com/example/neatboard1/service/ArticleCommentServiceTest.java
@@ -7,6 +7,7 @@ import com.example.neatboard1.dto.ArticleCommentDto;
 import com.example.neatboard1.dto.UserAccountDto;
 import com.example.neatboard1.repository.ArticleCommentRepository;
 import com.example.neatboard1.repository.ArticleRepository;
+import com.example.neatboard1.repository.UserAccountRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -32,6 +33,8 @@ class ArticleCommentServiceTest {
     @Mock
     private ArticleRepository articleRepository;
     @Mock private ArticleCommentRepository articleCommentRepository;
+    @Mock
+    private UserAccountRepository userAccountRepository;
 
     @DisplayName("게시글 ID로 조회하면, 해당하는 댓글 리스트를 반환한다.")
     @Test
@@ -57,6 +60,7 @@ class ArticleCommentServiceTest {
         // Given
         ArticleCommentDto dto = createArticleCommentDto("댓글");
         given(articleRepository.getReferenceById(dto.articleId())).willReturn(createArticle());
+        given(userAccountRepository.getReferenceById(dto.userAccountDto().userId())).willReturn(createUserAccount());
         given(articleCommentRepository.save(any(ArticleComment.class))).willReturn(null);
 
         // When
@@ -64,6 +68,7 @@ class ArticleCommentServiceTest {
 
         // Then
         then(articleRepository).should().getReferenceById(dto.articleId());
+        then(userAccountRepository).shouldHaveNoInteractions();
         then(articleCommentRepository).should().save(any(ArticleComment.class));
     }
 
