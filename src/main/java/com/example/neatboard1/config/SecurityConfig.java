a479deadacb017906f24c442d23e092354d77380
Changho0514 <chh9450@naver.com>
2024-01-02 18:25:12 +0900

    #39 - 시큐리티 설정 잡기
    모든 요청에 대해 `permit` 했었지만
    로그인이 필요한 페이지로 나누기 위해서 추가함.
    `UserDetailService` 는 실제 DB에 등록되어 있는 유저 정보를 가져온다.

--- src/main/java/com/example/neatboard1/config/SecurityConfig.java
+++ src/main/java/com/example/neatboard1/config/SecurityConfig.java
@@ -1,9 +1,23 @@
 package com.example.neatboard1.config;
 
+import com.example.neatboard1.dto.UserAccountDto;
+import com.example.neatboard1.dto.security.BoardPrincipal;
+import com.example.neatboard1.repository.UserAccountRepository;
+import jakarta.servlet.DispatcherType;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
@@ -11,8 +25,29 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()).formLogin().and()
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        .requestMatchers("/", "/articles", "/articles/search-hashtag").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(withDefaults())
+                .logout((logout) -> logout.logoutSuccessUrl("/"))
                 .build();
-
     }
+
+    // 인증 정보, 사용자 정보를 가져오는 메서드
+    @Bean
+    public UserDetailsService userDetailsService(UserAccountRepository userAccountRepository) {
+        return username -> userAccountRepository
+                .findById(username)
+                .map(UserAccountDto::from)
+                .map(BoardPrincipal::from)
+                .orElseThrow(() -> new UsernameNotFoundException("유저를 찾을 수 없습니다 - username: " + username));
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
 }
